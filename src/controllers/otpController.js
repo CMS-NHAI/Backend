b696a5691a2f42a7998967ca6a70354a08ee97cf
amit_chaman <amit_chaman@AmitChaman.cms.co.in>
2025-01-07 17:40:04 +0530

    added changes

--- src/controllers/otpController.js
+++ src/controllers/otpController.js
@@ -3,27 +3,30 @@ import prisma  from "../config/prismaClient.js";
 import sendOTP from "../services/twilioService.js"
 import  generateOTP  from "../utils/otpGenerator.js"; 
 import  validatePhoneNumber  from "../utils/validation.js";
+import { v4 as uuidv4 } from 'uuid';
 
+// Generate a UUID as the unique username
+const uniqueUsername = uuidv4();
 
 // const prismaClient = new prisma();
 //console.log(' prisma client ', prisma);
 export const sendOtpToUser = async (req, res) => {
-  const { phone_number } = req.body;
+  const { mobile_number } = req.body;
   const serviceSid = process.env.TWILIO_VERIFY_SERVICE_SID;
 
-  if (!phone_number) {
+  if (!mobile_number) {
     return res.status(400).json({
       success: false,
       status: 400,
-      message: 'Phone number is required.',
+      message: 'Mobile number is required.',
     });
   }
 
-  if (!validatePhoneNumber(phone_number)) {
+  if (!validatePhoneNumber(mobile_number)) {
     return res.status(400).json({
       success: false,
       status: 400,
-      message: 'Phone number must be in the format +91 followed by exactly 10 digits.',
+      message: 'Mobile number must be in the format +91 followed by exactly 10 digits.',
     });
   }
 
@@ -33,32 +36,34 @@ export const sendOtpToUser = async (req, res) => {
     // });
     console.log(prisma);
     const user = await prisma.user_master.findUnique({  // Use correct model name
-        where: { phone_number: phone_number },  // Assuming `mobile_number` is the field to search
+        where: { mobile_number: mobile_number},  // Assuming `mobile_number` is the field to search
       });
     console.log(user);
     if (!user) {
-        u = await prisma.user_master.create({
-            data: {
-              phone_number: phone_number,
-              name: 'Shailendra',  // Dummy name or leave it blank
-              first_name: 'Shailendra',  // First name can also be default
-              user_type: 'UNREGISTERED',  // A type to indicate the user is not yet registered
-              user_role: { role: 'UNREGISTERED' },  // You can set a default role
-              email: 'amit@gmail.com',  // No email for a dummy user
-              is_active: false,  // Set to false for unregistered users
-              is_kyc_verified: false,  // Assuming KYC is not verified for dummy users
-              otp: generateOTP(),  // Generate OTP
-              otp_timestamp: new Date(),
-              created_at: new Date(),
-              updated_at: new Date(),
-              // Optional: You can fill in additional fields if needed (e.g., `activation_status`)
-              activation_status: 'PENDING', 
-              unique_username : "Amit",
-              sap_id : "SAP1234",
-              Aadhar_image : "HTTP",
-              User_image : "HEE" // Status indicating the user is pending activation
-            },
-          });
+        // u = await prisma.user_master.create({
+        //     data: {
+        //       // unique_username: uuidv4(),
+        //       mobile_number: mobile_number,
+        //       name: 'Shailendra',  // Dummy name or leave it blank
+        //       first_name: 'Shailendra',  // First name can also be default
+        //       user_type: 'Internal - Permanent',  // A type to indicate the user is not yet registered
+        //       user_role: { role: 'UNREGISTERED' },  // You can set a default role
+        //       email: 'amit@gmail.com',  // No email for a dummy user
+        //       is_active: false,  // Set to false for unregistered users
+        //       is_kyc_verified: false,  // Assuming KYC is not verified for dummy users
+        //       otp: generateOTP(),  // Generate OTP
+        //       otp_timestamp: new Date(),
+        //       created_at: new Date(),
+        //       updated_at: new Date(),
+        //       // Optional: You can fill in additional fields if needed (e.g., `activation_status`)
+        //       activation_status: 'Pending', 
+        //       organization_id: 1,
+        //       unique_username : uniqueUsername,
+        //       sap_id : "SAP1234",
+        //       aadhar_image : "HTTP",
+        //       user_image : "HEE" // Status indicating the user is pending activation
+        //     },
+        //   });
       return res.status(400).json({
         success: false,
         status: 400,
@@ -71,11 +76,11 @@ export const sendOtpToUser = async (req, res) => {
     const otp_timestamp = new Date();
 
     await prisma.user_master.update({
-      where: { phone_number },
+      where: { mobile_number },
       data: { otp, otp_timestamp },
     });
 
-    await sendOTP(serviceSid, phone_number, otp);
+    await sendOTP(serviceSid, mobile_number, otp);
 
     res.status(200).json({
       success: true,
@@ -84,6 +89,7 @@ export const sendOtpToUser = async (req, res) => {
     });
 
   } catch (err) {
+    console.log(err);
     console.error(err);
     res.status(500).json({
       success: false,
@@ -93,4 +99,4 @@ export const sendOtpToUser = async (req, res) => {
   }
 };
 
- export default sendOtpToUser
+ export default sendOtpToUser;

67415eedecb96e7e97f5e008278736bf4cb568d8
amit_chaman <amit_chaman@AmitChaman.cms.co.in>
2025-01-07 22:57:04 +0530

    changed request url

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.post('/verify-otp', verifyOtp);
 router.post('/signup',signup );
-router.post('/getUserDetails', getUserDetails);
+router.post('/get-user-details', getUserDetails);
 router.post('/getsap', getSapDetails);
 
 export default router;

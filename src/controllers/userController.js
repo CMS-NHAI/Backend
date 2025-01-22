948776849e63a92c010a68311d432ae5942548e6
amit_chaman <amit_chaman@AmitChaman.cms.co.in>
2025-01-22 15:52:50 +0530

    get user api created

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,6 +11,7 @@ import { createUserValidationSchema } from "../validations/createUserValidation.
 import { updateUserStatusValidationSchema } from "../validations/updateUserStatusValidation.js";
 import { updateUserValidationSchema } from '../validations/updateUserValidation.js';
 import { inviteUserValidationSchema } from '../validations/inviteUserValidation.js';
+import { userIdValidation } from '../validations/getUserValidation.js';
 import { v4 as uuidv4 } from 'uuid';
 import crypto from "crypto";
 
@@ -900,7 +901,7 @@ export const createInvitation = async (req, res) =>{
 
 }   
       
-  export const inviteUser = async (req, res) => {
+export const inviteUser = async (req, res) => {
 
     const {
       name,
@@ -914,7 +915,7 @@ export const createInvitation = async (req, res) =>{
       contracts,
       roles_permission
       } = req.body;
-  
+      const uniqueUsername2 = uuidv4();
     const { error } = inviteUserValidationSchema.validate(req.body);
 
     if (error) {
@@ -950,7 +951,7 @@ export const createInvitation = async (req, res) =>{
           user_type,
           status,
           created_at: new Date(), 
-          unique_username : uniqueUsername,
+          unique_username : uniqueUsername2,
           user_role,
           aadhar_image,
           user_image,
@@ -978,6 +979,61 @@ export const createInvitation = async (req, res) =>{
     }
   }
 
+  export const getUserById = async (req, res) => {
+    const { user_id } = req.body;
+  
+    // Validate user_id using Joi validation schema
+    const { error } = userIdValidation.validate(req.body);
+  
+    if (error) {
+      return res.status(STATUS_CODES.BAD_REQUEST).json({
+        success: false,
+        status: 400,
+        message: error.details[0].message,
+      });
+    }
+  
+    try {
+      // Find user by user_id
+      const user = await prisma.user_master.findUnique({
+        where: {
+          user_id: user_id, // Fetch user using user_id
+        },
+      });
+  
+      // If the user is not found
+      if (!user) {
+        return res.status(STATUS_CODES.NOT_FOUND).json({
+          success: false,
+          status: 404,
+          message: "User not found.",
+        });
+      }
+  
+      // Return the user data if found
+      res.status(STATUS_CODES.OK).json({
+        success: true,
+        status: 200,
+        message: "User fetched successfully.",
+        data: user,
+      });
+    } catch (error) {
+      // Handle unexpected errors
+      console.error("Error fetching user:", error);
+      res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        status: 500,
+        message: error.message,
+      });
+    }
+  };
+  
+  
+
+    
+    
+    
+  
 
 
 

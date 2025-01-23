102a2e246a7645b2e349bf8075905b82748f98b8
yogeshwar <yogeshwar_parth@cms.co.in>
2025-01-23 18:28:03 +0530

    role controller yogesh

--- src/routes/keycloak/roleRoute.js
+++ src/routes/keycloak/roleRoute.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import { keycloakRoleList, keycloakaddRole } from '../../controllers/keycloak/roleController.js'
+import { keycloakRoleList, keycloakaddRole, keycloakaddRoleResourceScopePolicyPermission, updateMultipleResourceScopes, updateSingleResourceScopes } from '../../controllers/keycloak/roleController.js'
 
 const router = Router();
 
 router.get('/list', keycloakRoleList);
 router.post('/create', keycloakaddRole);
-
+router.post('/permission', keycloakaddRoleResourceScopePolicyPermission);
+router.post('/multiple-resource-scope/update', updateMultipleResourceScopes);
+router.post('/single-resource-scope/update', updateSingleResourceScopes);
 
 export default router;

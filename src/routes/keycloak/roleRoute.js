a3d171cc6f2f9b0e1e842e461a8dba33ec46d24f
bhawesh-bhanu <bhawesh_bhanu@cms.co.in>
2025-02-04 01:00:01 +0530

    Revert "Updated Role Route"

--- src/routes/keycloak/roleRoute.js
+++ src/routes/keycloak/roleRoute.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 
-import { keycloakaddRoleResourceScopePolicyPermission, updateRoleAndScopes, keycloakRoleResourecScopeList, keycloakRoleList, keycloakaddRole, updateSingleResourceScopes , keycloakResourceDetail} from '../../controllers/keycloak/roleController.js'
+import { keycloakaddRoleResourceScopePolicyPermission, updateResourceScopes, keycloakRoleResourecScopeList, keycloakRoleList, keycloakaddRole, updateSingleResourceScopes , keycloakResourceDetail} from '../../controllers/keycloak/roleController.js'
 
 const router = Router();
 
 
 router.post('/permission/create', keycloakaddRoleResourceScopePolicyPermission);
-router.put('/scope/update', updateRoleAndScopes);
+router.put('/scope/update', updateResourceScopes);
 router.get('/resource/scope', keycloakRoleResourecScopeList);
 //===============================================
 router.post('/resource-scope/update', updateSingleResourceScopes);

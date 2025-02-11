891067aae2b548c5487c68f8712dd9880fb08f6e
Your Name <bhawesh_bhanu@cms.co.in>
2025-02-11 12:00:24 +0530

    bulkRegistration API

---
+++ src/controllers/bulkRegistrationController.js
@@ -0,0 +1,22 @@
+import prisma from "../../config/prismaClient.js";
+import jwt from "jsonwebtoken";
+import { STATUS_CODES } from "../../constants/statusCodesConstant.js";
+import { v4 as uuidv4 } from 'uuid';
+import crypto from 'crypto';
+import {sendEmail} from '../../services/emailService.js';
+
+
+export const createBulkAgency = async (req, res) => {
+
+}
+
+
+
+export const createBulkAgencyUser = async (req, res) => {
+
+}
+
+
+export const createBulkParmanentUser = async (req, res) => {
+
+}

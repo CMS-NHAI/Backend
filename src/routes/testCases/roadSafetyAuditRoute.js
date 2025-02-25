7a1415451bbb8b4e9a0296a4f450128745d551cc
yogeshwar <yogeshwar_parth@cms.co.in>
2025-02-25 14:34:41 +0530

    Updated roadSafety

--- src/routes/testCases/roadSafetyAuditRoute.js
+++ src/routes/testCases/roadSafetyAuditRoute.js
@@ -7,6 +7,6 @@ import checkToken from '../../middlewares/checkToken.js';
 router.get('/view', checkToken, userRoleResourcePermissionBasedAccess(['PD', 'RO', 'Deputy Manager', 'Manager', 'DGM', 'Other Staff'], ['Road Safety Audit'], ['View']), viewRoadSafetyAudit)
 router.post('/recommend', checkToken, userRoleResourcePermissionBasedAccess(['PD'], ['Road Safety Audit'], ['Recommend']), recommendRoadSafetyAudit)
 router.post('/approve', checkToken, userRoleResourcePermissionBasedAccess(['RO'], ['Road Safety Audit'], ['Approve']), approveRoadSafetyAudit)
-router.post('/suggest', checkToken, userRoleResourcePermissionBasedAccess(['Deputy Manager', 'Manager', 'DGM', 'Other Staff'], ['Road Safety Audit'], ['Suggest']), suggestRoadSafetyAudit)
+router.post('/suggest', checkToken, userRoleResourcePermissionBasedAccess(['Other Staff'], ['Road Safety Audit'], ['Suggest']), suggestRoadSafetyAudit)
 
 export default router

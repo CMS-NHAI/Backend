a45a65f98fea09f50de4ec4d981973fc28d5fd7b
Your Name <bhawesh_bhanu@cms.co.in>
2025-02-27 14:58:00 +0530

    parser changes limit

--- src/index.js
+++ src/index.js
@@ -42,12 +42,11 @@ i18n.configure({
 
 app.use(i18n.init);
 app.use(helmet());
-app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(cors({ origin: '*' }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "10mb" }));
 
-app.use(express.json({ limit: "10mb" })); 
+//app.use(express.json({ limit: "10mb" })); 
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
  
 app.use('/api/v1/otp', router);

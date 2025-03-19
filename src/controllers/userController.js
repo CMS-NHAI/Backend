a57de9febf86a39e95629b14f7193ff160e43d90
Your Name <bhawesh_bhanu@cms.co.in>
2025-03-19 18:23:28 +0530

    agency and user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1590,8 +1590,7 @@ export const getRegistrationInfoByInviteId = async(req, res) =>{
 
         if(registerInfo.invitation_type == "User"){
           const inviteUser = await prisma.user_master.findUnique({
-            where: { user_id: registerInfo.user_id,
-                     invitation_type: "User"
+            where: { user_id: registerInfo.user_id
              }
           }); 
           
@@ -1608,8 +1607,7 @@ export const getRegistrationInfoByInviteId = async(req, res) =>{
 
         if(registerInfo.invitation_type == "Agency"){
           const inviteagency = await prisma.organization_master.findUnique({ 
-            where: { org_id: registerInfo.org_id,
-              invitation_type: "Agency"
+            where: { org_id: registerInfo.org_id
              } 
           });
           

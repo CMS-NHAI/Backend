195816a27bfb304c5af3784f282f5d0f499539b0
bhawesh-bhanu <bhawesh_bhanu@cms.co.in>
2025-03-25 15:05:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ app.get("/.well-known/assetlinks.json", (req, res) => {
 //app.use("/api/v1/article", ArticleRouter);
 //app.use("/api/v1/user", UserRouter);
 
-app.get('/backend', (req, res) => {
+app.get('/backend/auth', (req, res) => {
   res.status(STATUS_CODES.OK).send({
     message: `Welcome to Datalake 3.0 ${APP_CONSTANTS.APP_NAME} v${APP_CONSTANTS.VERSION}`,
   });
